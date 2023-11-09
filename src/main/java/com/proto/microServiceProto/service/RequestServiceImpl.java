43b0ee04d7776902f70034ef63099f89cac35da1
a.turki <at.aslem@gmail.com>
2023-11-09 14:05:28 +0100

    proto V3 webClient request

--- src/main/java/com/proto/microServiceProto/service/RequestServiceImpl.java
+++ src/main/java/com/proto/microServiceProto/service/RequestServiceImpl.java
@@ -1,12 +1,28 @@
 package com.proto.microServiceProto.service;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Service
 public class RequestServiceImpl implements RequestService {
 
     @Override
     public String getAllRequest() {
-        return "hello";
+        return webClientRequest();
+    }
+
+    private String webClientRequest(){
+        WebClient client = WebClient.create("http://localhost:8089");
+        String result = client.get()
+                .uri("/api/Typo/getRequest")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        System.out.println(result);
+
+        return result;
     }
 }
---
+++ src/test/java/com/proto/microServiceProto/MicroServiceProtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proto.microServiceProto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceProtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
